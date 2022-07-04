3b9f550ebe07dcf8a00bc7f8277191fbc27fe016
Dominik Ryńko <dominik.rynko@lppsa.com>
2022-07-04 14:13:04 +0200

    Exclude frame config to separate class

---
+++ src/Config/FrameConfig.java
@@ -0,0 +1,8 @@
+package Config;
+
+public class FrameConfig
+{
+    public static final String TITLE = "My Weather Online";
+    public static final int WINDOW_WIDTH = 400;
+    public static final int WINDOW_HEIGHT = 400;
+}
--- src/UI/AppFrame.java
+++ src/UI/AppFrame.java
@@ -1,17 +1,16 @@
 package UI;
 
+import Config.FrameConfig;
+
 import javax.swing.*;
 
 public class AppFrame implements Runnable
 {
-    private static final int WINDOW_WIDTH = 400;
-    private static final int WINDOW_HEIGHT = 400;
-
     public void run()
     {
-        JFrame frame = new JFrame("My Weather");
+        JFrame frame = new JFrame(FrameConfig.TITLE);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        frame.setSize(FrameConfig.WINDOW_WIDTH, FrameConfig.WINDOW_HEIGHT);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }

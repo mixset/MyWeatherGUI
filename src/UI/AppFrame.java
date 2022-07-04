29568b95616d7e3121406efbd48c3cdd24e7fedc
Dominik <dominikrynko@gmail.com>
2022-07-04 22:08:17 +0200

    Work with GUI

--- src/UI/AppFrame.java
+++ src/UI/AppFrame.java
@@ -7,6 +7,12 @@ import java.awt.*;
 
 public class AppFrame implements Runnable
 {
+    // frame
+    static JFrame f;
+
+    // text areas
+    static JTextArea t1, t2;
+
     public void run()
     {
         JFrame frame = new JFrame(FrameConfig.TITLE);
@@ -15,5 +21,21 @@ public class AppFrame implements Runnable
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         frame.setIconImage(Toolkit.getDefaultToolkit().getImage("resources/icon.png"));
+
+        JPanel MyPanel1 = new JPanel();
+
+        MyPanel1.setLayout( new BorderLayout() );
+
+        JButton x1 = new JButton("I am x1");
+
+        MyPanel1.add(x1, "North");
+
+        frame.getContentPane().add( MyPanel1, "North");
+
+        JPanel MyPanel2 = new JPanel();
+
+        MyPanel2.setLayout( new BorderLayout() );
+
+        frame.getContentPane().add( MyPanel2, "South");
     }
 }

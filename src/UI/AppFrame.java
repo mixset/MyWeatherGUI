adcc25d6ad9741a1a3ecdebeab4c47ef51501d90
Dominik <dominikrynko@gmail.com>
2022-07-24 20:17:59 +0200

    Exclude action performed to sepearted file and add translations

--- src/UI/AppFrame.java
+++ src/UI/AppFrame.java
@@ -1,22 +1,15 @@
 package UI;
 
 import Config.FrameConfig;
-import DTO.Request;
-import DTO.Weather;
-import Http.WeatherDataProvider;
-import org.json.simple.parser.ParseException;
+import UI.Listener.ActionPerformedListener;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import Exception.WeatherNotFoundException;
 
-public class AppFrame implements Runnable, ActionListener
+public class AppFrame implements Runnable
 {
     JFrame frame;
+    JPanel formPanel;
     JPanel bottomPanel;
     JTextField city;
     JLabel cityLabel;
@@ -34,66 +27,33 @@ public class AppFrame implements Runnable, ActionListener
 
         JSplitPane panel = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
 
-        JPanel formPanel = new JPanel();
+        formPanel = new JPanel();
         formPanel.setBackground(new Color(215, 241, 250));
 
-        JLabel formLabel = new JLabel("Wpisz nazwę miasta, aby pobrać pogodę:");
+        bottomPanel = new JPanel();
+        bottomPanel.setBackground(new Color(245, 245, 245));
+
+        JLabel formLabel = new JLabel(Translation.APP_HEADER);
         city = new JTextField(15);
-        JButton button = new JButton("Pobierz");
-        button.addActionListener(this);
+
+        JButton button = new JButton(Translation.BUTTON_NAME);
+        button.addActionListener(new ActionPerformedListener(cityLabel, temperatureLabel, windLabel, bottomPanel, frame, city));
 
         formPanel.add(formLabel);
         formPanel.add(city);
         formPanel.add(button);
 
-        bottomPanel = new JPanel();
-        bottomPanel.setBackground(new Color(245, 245, 245));
-
-        JLabel defaultLabel = new JLabel ("Uzupełnij formularz, aby wyświetlic dane pogodowe.");
+        JLabel defaultLabel = new JLabel(Translation.DEFAULT_PANEL_MESSAGE);
 
         bottomPanel.add(defaultLabel);
 
         panel.add(formPanel);
         panel.add(bottomPanel);
+
         panel.setDividerSize(0);
         panel.setResizeWeight(0.15);
 
         frame.getContentPane().add(panel);
         frame.setVisible(true);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e)
-    {
-        String action = e.getActionCommand();
-
-        if (action.equals("Pobierz")) {
-            WeatherDataProvider weatherDataProvider = new WeatherDataProvider();
-            try {
-                Weather weather = weatherDataProvider.execute(new Request(city.getText().trim()));
-
-                cityLabel = new JLabel(
-                    String.format(
-                        "Miasto: %s(%.3f, %.3f) \n",
-                        weather.getLocation().getCity(),
-                        weather.getLocation().getLatency(),
-                        weather.getLocation().getLongitude()
-                    )
-                );
-                temperatureLabel = new JLabel (String.format("Temperatura: %s stopni Celsiusa \n", weather.getTemperature().getTemperature()));
-                windLabel = new JLabel (String.format("Wiatr: %f km/h, kierunek: %s \n", weather.getWind().getSpeed(), weather.getWind().getDirection()));
-
-                bottomPanel.removeAll();
-
-                bottomPanel.add(cityLabel);
-                bottomPanel.add(temperatureLabel);
-                bottomPanel.add(windLabel);
-
-                frame.revalidate();
-                frame.repaint();
-            } catch (IOException | InterruptedException | ParseException | URISyntaxException | WeatherNotFoundException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
     }
 }
---
+++ src/UI/Listener/ActionPerformedListener.java
@@ -0,0 +1,81 @@
+package UI.Listener;
+
+import DTO.Request;
+import DTO.Weather;
+import Http.WeatherDataProvider;
+import UI.Translation;
+import org.json.simple.parser.ParseException;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import Exception.WeatherNotFoundException;
+
+public class ActionPerformedListener implements ActionListener
+{
+    private JLabel cityLabel;
+    private JLabel temperatureLabel;
+    private JLabel windLabel;
+    private final JPanel bottomPanel;
+    private final JFrame frame;
+    private final JTextField cityTextField;
+
+    public ActionPerformedListener(
+        JLabel cityLabel,
+        JLabel temperatureLabel,
+        JLabel windLabel,
+        JPanel bottomPanel,
+        JFrame frame,
+        JTextField cityTextField
+    ) {
+        this.cityLabel = cityLabel;
+        this.temperatureLabel = temperatureLabel;
+        this.windLabel = windLabel;
+        this.bottomPanel = bottomPanel;
+        this.frame = frame;
+        this.cityTextField = cityTextField;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        String action = e.getActionCommand();
+
+        if (action.equals(Translation.BUTTON_NAME)) {
+            WeatherDataProvider weatherDataProvider = new WeatherDataProvider();
+
+            bottomPanel.removeAll();
+
+            try {
+                Weather weather = weatherDataProvider.execute(new Request(cityTextField.getText().trim()));
+
+                cityLabel = new JLabel(
+                        String.format(
+                            Translation.CITY_FORMAT,
+                            weather.getLocation().getCity(),
+                            weather.getLocation().getLatency(),
+                            weather.getLocation().getLongitude()
+                        )
+                );
+                temperatureLabel = new JLabel (String.format(Translation.TEMPERATURE_FORMAT, weather.getTemperature().getTemperature()));
+                windLabel = new JLabel (String.format(Translation.WIND_FORMAT, weather.getWind().getSpeed(), weather.getWind().getDirection()));
+
+                bottomPanel.add(cityLabel);
+                bottomPanel.add(temperatureLabel);
+                bottomPanel.add(windLabel);
+            } catch (IOException | InterruptedException | ParseException | URISyntaxException ex) {
+                throw new RuntimeException(ex);
+            } catch (WeatherNotFoundException ex) {
+                JLabel cityNotFoundLabel = new JLabel(Translation.CITY_NO_DATA);
+
+                bottomPanel.removeAll();
+                bottomPanel.add(cityNotFoundLabel);
+            } finally {
+                frame.revalidate();
+                frame.repaint();
+            }
+        }
+    }
+}

f46b1d597dc9c35debb2426dc65c0be19b20fa49
Dominik <dominikrynko@gmail.com>
2022-07-24 14:02:30 +0200

    Working GUI

--- src/Config/FrameConfig.java
+++ src/Config/FrameConfig.java
@@ -4,5 +4,5 @@ public class FrameConfig
 {
     public static final String TITLE = "My Weather Online";
     public static final int WINDOW_WIDTH = 400;
-    public static final int WINDOW_HEIGHT = 400;
+    public static final int WINDOW_HEIGHT = 240;
 }
--- src/UI/AppFrame.java
+++ src/UI/AppFrame.java
@@ -1,41 +1,99 @@
 package UI;
 
 import Config.FrameConfig;
+import DTO.Request;
+import DTO.Weather;
+import Http.WeatherDataProvider;
+import org.json.simple.parser.ParseException;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import Exception.WeatherNotFoundException;
 
-public class AppFrame implements Runnable
+public class AppFrame implements Runnable, ActionListener
 {
-    // frame
-    static JFrame f;
-
-    // text areas
-    static JTextArea t1, t2;
+    JFrame frame;
+    JPanel bottomPanel;
+    JTextField city;
+    JLabel cityLabel;
+    JLabel temperatureLabel;
+    JLabel windLabel;
 
     public void run()
     {
-        JFrame frame = new JFrame(FrameConfig.TITLE);
+        frame = new JFrame(FrameConfig.TITLE);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(FrameConfig.WINDOW_WIDTH, FrameConfig.WINDOW_HEIGHT);
         frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
         frame.setIconImage(Toolkit.getDefaultToolkit().getImage("resources/icon.png"));
+        frame.setResizable(false);
 
-        JPanel MyPanel1 = new JPanel();
+        JSplitPane panel = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
 
-        MyPanel1.setLayout( new BorderLayout() );
+        JPanel formPanel = new JPanel();
+        formPanel.setBackground(new Color(215, 241, 250));
 
-        JButton x1 = new JButton("I am x1");
+        JLabel formLabel = new JLabel("Wpisz nazwę miasta, aby pobrać pogodę:");
+        city = new JTextField(15);
+        JButton button = new JButton("Pobierz");
+        button.addActionListener(this);
 
-        MyPanel1.add(x1, "North");
+        formPanel.add(formLabel);
+        formPanel.add(city);
+        formPanel.add(button);
 
-        frame.getContentPane().add( MyPanel1, "North");
+        bottomPanel = new JPanel();
+        bottomPanel.setBackground(new Color(245, 245, 245));
 
-        JPanel MyPanel2 = new JPanel();
+        JLabel defaultLabel = new JLabel ("Uzupełnij formularz, aby wyświetlic dane pogodowe.");
 
-        MyPanel2.setLayout( new BorderLayout() );
+        bottomPanel.add(defaultLabel);
 
-        frame.getContentPane().add( MyPanel2, "South");
+        panel.add(formPanel);
+        panel.add(bottomPanel);
+        panel.setDividerSize(0);
+        panel.setResizeWeight(0.15);
+
+        frame.getContentPane().add(panel);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        String action = e.getActionCommand();
+
+        if (action.equals("Pobierz")) {
+            WeatherDataProvider weatherDataProvider = new WeatherDataProvider();
+            try {
+                Weather weather = weatherDataProvider.execute(new Request(city.getText().trim()));
+
+                cityLabel = new JLabel(
+                    String.format(
+                        "Miasto: %s(%.3f, %.3f) \n",
+                        weather.getLocation().getCity(),
+                        weather.getLocation().getLatency(),
+                        weather.getLocation().getLongitude()
+                    )
+                );
+                temperatureLabel = new JLabel (String.format("Temperatura: %s stopni Celsiusa \n", weather.getTemperature().getTemperature()));
+                windLabel = new JLabel (String.format("Wiatr: %f km/h, kierunek: %s \n", weather.getWind().getSpeed(), weather.getWind().getDirection()));
+
+                bottomPanel.removeAll();
+
+                bottomPanel.add(cityLabel);
+                bottomPanel.add(temperatureLabel);
+                bottomPanel.add(windLabel);
+
+                frame.revalidate();
+                frame.repaint();
+            } catch (IOException | InterruptedException | ParseException | URISyntaxException | WeatherNotFoundException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
     }
 }
